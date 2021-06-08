c94a036cb7affe0a0fcbe5b2de8d4b6cb23f8c4e
eldrid01 <matthew.d.eldridge@gmail.com>
2021-06-08 16:00:41 +0100

    Added port argument to start_shiny_app function

--- R/shiny_app.R
+++ R/shiny_app.R
@@ -7,12 +7,13 @@
 #' @param load_sample_data load a sample data set on starting the Shiny app.
 #' @param launch_browser launch the application in a web browser on starting the
 #' Shiny app.
+#' @param port the TCP port that the application should listen on.
 #' @return opens the Shiny app in a web browser.
 #' @import shiny
 #' @export
-start_shiny_app <- function(load_sample_data = TRUE, launch_browser = TRUE) {
+start_shiny_app <- function(load_sample_data = TRUE, launch_browser = TRUE, port = getOption("shiny.port")) {
   dir <- system.file("shiny", package = "rascal")
   if (dir == "") stop("Could not find shiny app directory in the rascal package")
   shiny::shinyOptions(load_sample_data = load_sample_data)
-  shiny::runApp(dir, launch.browser = launch_browser, display.mode = "normal")
+  shiny::runApp(dir, port = port, launch.browser = launch_browser, display.mode = "normal")
 }
