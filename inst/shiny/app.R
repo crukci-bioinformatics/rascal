31f4a784e8db77666d09ffcc101e5545af4d8663
eldrid01 <matthew.d.eldridge@gmail.com>
2025-08-29 13:00:34 +0100

    Minor change to formatting for cookie notice (needs to be uncommented for CRUK CI deployment)

--- inst/shiny/app.R
+++ inst/shiny/app.R
@@ -447,7 +447,7 @@ ui <- fluidPage(
     # tags$div(style = "float:right", tags$a(href = "https://www.cam.ac.uk/about-this-site/terms-and-conditions", "Terms and Conditions"))
   ),
   # tags$div(style="line-height:25%;", br()),
-  # tags$div(style = "font-size:75%;", "The Institute's cookie policy can be found", tags$a(href = "https://content.cruk.cam.ac.uk/computing/cruk-domains.html", "here.")),
+  # tags$div(style = "font-size:75%; float:right;", "Cancer Research UK Cambridge Institute's cookie policy can be found", tags$a(href = "https://content.cruk.cam.ac.uk/computing/cruk-domains.html", "here."), p()),
   br()
 )
 
